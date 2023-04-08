04ffb70c0bcf330942f690a9164bef241ada2d2f
iambalakrishnan <imbkrishnaa@gmail.com>
2023-04-08 15:32:39 +0530

    logging updated

--- src/logs/log.py
+++ src/logs/log.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 
 LOG_FILE = f"{datetime.now().strftime('%m-%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join("running_logs",LOG_FILE)
+logs_path = os.path.join("artifacts","running_logs",LOG_FILE)
 
 
 os.makedirs(logs_path, exist_ok=True)
@@ -17,4 +17,3 @@ logging.basicConfig(
     format="[%(asctime)s: %(lineno)d: %(name)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a",
 )
-
