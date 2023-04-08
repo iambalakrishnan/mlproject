12aafa8597ff42a1afcba13624c33dc34b5e2885
iambalakrishnan <imbkrishnaa@gmail.com>
2023-04-08 13:48:14 +0530

    exception and logs tested

--- src/logs/log.py
+++ src/logs/log.py
@@ -3,8 +3,8 @@ import logging
 from datetime import datetime
 
 
-LOG_FILE = f"{datetime.now().strftime('%m-%d_%Y_%H:%M:%S')}.log"
-logs_path = os.path.join("running_logs", "logs",LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%m-%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join("running_logs","logs",LOG_FILE)
 
 
 os.makedirs(logs_path, exist_ok=True)
@@ -12,8 +12,8 @@ os.makedirs(logs_path, exist_ok=True)
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH
+    filename=LOG_FILE_PATH,
     level=logging.INFO,
-    format="[%(asctime)s: %(lineno)d: %(name)s: %(levelname)s: %(module)s]: %(message)s",
+    format="[%(asctime)s: %(levelname)s: %(module)s]: %(message)s",
     filemode="a",
 )
