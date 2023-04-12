eea9f38fc0022857fcb82c28ab6f2dbf1f2b7cd9
iambalakrishnan <imbkrishnaa@gmail.com>
2023-04-12 16:17:27 +0530

    hyperparameter implemented

--- src/utils/util.py
+++ src/utils/util.py
@@ -6,6 +6,7 @@ import pandas as pd
 import dill
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import r2_score
 from src.exceptions.exception import CustomException
 from src.logs.log import logging
@@ -25,13 +26,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test,y_test,models):
+def evaluate_model(X_train, y_train, X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
+            grid = GridSearchCV(model, para, cv=3)
+            grid.fit(X_train, y_train)
+            
+            model.set_params(**grid.best_params_)
             model.fit(X_train, y_train)
 
             y_train_pred = model.predict(X_train)
